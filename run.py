0311163a9be6cc0c9cf683144b6db86718f1651a
Fatema Mahabub <fmahabub8@gmail.com>
2024-01-27 09:28:18 +0000

    display the guessed letter

--- run.py
+++ run.py
@@ -1,46 +1,38 @@
 import random
 
 def choose_random_city():
-    # List of cities in Germany
     cities = ["Berlin", "Hamburg", "Munich", "Cologne", "Frankfurt", "Stuttgart", "Dusseldorf", "Dortmund", "Essen",
               "Leipzig", "Bremen", "Dresden", "Hanover", "Nuremberg", "Duisburg", "Bochum", "Wuppertal", "Bielefeld",
               "Bonn", "Mannheim", "Karlsruhe", "Wiesbaden", "Munster", "Gelsenkirchen", "Augsburg", "Moenchengladbach",
               "Braunschweig", "Chemnitz", "Kiel", "Aachen", "Magdeburg", "Halle", "Freiburg", "Krefeld", "Lubeck",
               "Oberhausen", "Erfurt", "Mainz", "Rostock", "Kassel", "Hagen", "Saarbrucken", "Hamm", "Potsdam"]
-    
-    # Return a randomly chosen city in uppercase
     return random.choice(cities).upper()
 
-# Get a random city
-random_city = choose_random_city()
-print(random_city)
+def display_word(word, guessed_letters):
+    display = ""
+    for letter in word:
+        if letter in guessed_letters:
+            display += letter
+        else:
+            display += "_"
+    return display
 
-
-
-# Call the hangman function
 def hangman():
-    # Get player name from input
     player_name = input("Set your name: ")
 
-    # Check if no name is provided, default to "Guest"
     if not player_name:
         player_name = "Guest"
         print("No name provided. Defaulting to Guest.")
 
-    # Welcome message
     print(f"Welcome, {player_name}, to Hangman - Guess the City in Germany!")
 
-# Get a random city to guess
-city_to_guess = choose_random_city()
+    city_to_guess = choose_random_city()
+    guessed_letters = []
+    attempts = 6
 
-# Initialize variables for guessed letters and attempts
-guessed_letters = []
-attempts = 5
+    print("Here's a hint: First two letters are", city_to_guess[:2])
 
-# Display a hint for the first two letters of the city
-print("Here's a hint: First two letters are", city_to_guess[:2])
-
-while attempts > 0:
+    while attempts > 0:
         print("\nAttempts left:", attempts)
         print("Current Word:", display_word(city_to_guess, guessed_letters))
 
@@ -73,4 +65,4 @@ while attempts > 0:
         print(f"Sorry, {player_name}, you ran out of attempts. The correct city was:", city_to_guess)
 
 if __name__ == "__main__":
-hangman()
+    hangman()
